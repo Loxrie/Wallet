908066e52d9ee2ff581200d21e1dbf14d027b2fa
Duff Neubauer <d.neubauer@me.com>
2015-10-18 10:32:22 -0700

    Initial Commit

---
+++ Wallet/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Wallet
+//
+//  Created by Duff Neubauer on 10/18/15.
+//  Copyright © 2015 Duff Neubauer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
