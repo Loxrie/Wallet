8087017d0d767424dd6ff5619eb40cb2b0683c60
Duff Neubauer <d.neubauer@me.com>
2015-12-13 15:06:11 -0800

    SplitView stuff is set up. No bank account or budget stuff

--- Wallet/SourceListViewController.swift
+++ Wallet/SourceListViewController.swift
@@ -8,9 +8,120 @@
 
 import Cocoa
 
+//========================================================================================
+// MARK: - SourceListViewControllerDelegate
+//========================================================================================
+protocol SourceListViewControllerDelegate {
+  func displayViewController(viewController: NSViewController)
+}
+
+
+//========================================================================================
+// MARK: - SourceListViewController
+//========================================================================================
 class SourceListViewController: NSViewController, NSOutlineViewDataSource {
   
-  //========================================================================================
+  @IBOutlet weak var outlineView: NSOutlineView!
+  var sourceItems = [SourceListItem]()
+  var delegate: SourceListViewControllerDelegate?
+  
+  //----------------------------------------------------------------------------------------
+  // refresh
+  //----------------------------------------------------------------------------------------
+  func refresh() {
+    self.outlineView.reloadData()
+    self.outlineView.expandItem(nil, expandChildren: true)
+  }
+  
   // MARK: - NSOutlineViewDataSource
-  //========================================================================================
+  
+  //----------------------------------------------------------------------------------------
+  // outlineView(outlineView:numberOfChildrenOfItem:) -> Int
+  //----------------------------------------------------------------------------------------
+  func outlineView(outlineView: NSOutlineView, numberOfChildrenOfItem item: AnyObject?) -> Int {
+    guard let sourceItem = item as? SourceListItem else { return self.sourceItems.count }
+    
+    return sourceItem.children.count
+  }
+  
+  //----------------------------------------------------------------------------------------
+  // outlineView(outlineView:isItemExpandable:) -> Bool
+  //----------------------------------------------------------------------------------------
+  func outlineView(outlineView: NSOutlineView, isItemExpandable item: AnyObject) -> Bool {
+    guard let sourceItem = item as? SourceListItem else { return false }
+    
+    return sourceItem.hasChildren()
+  }
+  
+  //----------------------------------------------------------------------------------------
+  // outlineView(outlineView:child:ofItem:) -> AnyObject
+  //----------------------------------------------------------------------------------------
+  func outlineView(outlineView: NSOutlineView, child index: Int, ofItem item: AnyObject?) -> AnyObject {
+    guard let sourceItem = item as? SourceListItem else { return self.sourceItems[index] }
+    
+    return sourceItem.children[index]
+  }
+  
+  //----------------------------------------------------------------------------------------
+  // outlineView(outlineView:objectValueForTableColumn:byItem:) -> AnyObject?
+  //----------------------------------------------------------------------------------------
+  func outlineView(outlineView: NSOutlineView, objectValueForTableColumn tableColumn: NSTableColumn?, byItem item: AnyObject?) -> AnyObject? {
+    guard let sourceItem = item as? SourceListItem else { return nil }
+    
+    return sourceItem
+  }
+  
+  //----------------------------------------------------------------------------------------
+  // outlineView(outlineView:shouldEditTableColumn:item:) -> Bool
+  //----------------------------------------------------------------------------------------
+  func outlineView(outlineView: NSOutlineView, shouldEditTableColumn tableColumn: NSTableColumn?, item: AnyObject) -> Bool {
+    return false
+  }
+  
+  //----------------------------------------------------------------------------------------
+  // outlineView(outlineView:isGroupItem:) -> Bool
+  //----------------------------------------------------------------------------------------
+  func outlineView(outlineView: NSOutlineView, isGroupItem item: AnyObject) -> Bool {
+    guard let sourceItem = item as? SourceListItem else { return false }
+    
+    return sourceItem.isGroupItem
+  }
+  
+  //----------------------------------------------------------------------------------------
+  // outlineView(outlineView:shouldSelectItem:) -> Bool
+  //----------------------------------------------------------------------------------------
+  func outlineView(outlineView: NSOutlineView, shouldSelectItem item: AnyObject) -> Bool {
+    guard let sourceItem = item as? SourceListItem else { return false }
+    
+    return !sourceItem.isGroupItem
+  }
+  
+  //----------------------------------------------------------------------------------------
+  // outlineView(outlineView:viewForTableColumn:item:) -> NSView?
+  //----------------------------------------------------------------------------------------
+  func outlineView(outlineView: NSOutlineView, viewForTableColumn tableColumn: NSTableColumn?, item: AnyObject) -> NSView? {
+    guard let sourceItem = item as? SourceListItem else { return nil }
+    var cell: NSTableCellView? = nil
+    if sourceItem.isGroupItem {
+      cell = outlineView.makeViewWithIdentifier("HeaderCell", owner: self) as? NSTableCellView
+    } else {
+      cell = outlineView.makeViewWithIdentifier("DataCell", owner: self) as? NSTableCellView
+    }
+    
+    cell?.textField?.stringValue = sourceItem.title
+    
+    return cell
+  }
+  
+  //----------------------------------------------------------------------------------------
+  // outlineViewSelectionDidChange(notification:)
+  //----------------------------------------------------------------------------------------
+  func outlineViewSelectionDidChange(notification: NSNotification) {
+    let index = self.outlineView.selectedRow
+    guard let item  = self.outlineView.itemAtRow(index) as? SourceListItem else { return }
+    
+    if let content = item.viewController {
+      self.delegate?.displayViewController(content)
+    }
+  }
 }
--- Wallet/SplitViewController.swift
+++ Wallet/SplitViewController.swift
@@ -8,11 +8,115 @@
 
 import Cocoa
 
-class SplitViewController: NSSplitViewController {
+class SplitViewController: NSSplitViewController, SourceListViewControllerDelegate {
+  
+  // Storyboard IDs
+  let oneViewControllerID   = "ONE"
+  let twoViewControllerID   = "TWO"
+  let threeViewControllerID = "THREE"
+  
+  lazy var openPanel: NSOpenPanel = {
+    let panel = NSOpenPanel()
+    panel.title = "Open Quicken file."
+    panel.showsResizeIndicator = true
+    panel.showsHiddenFiles = false
+    panel.canChooseDirectories = false
+    panel.allowsMultipleSelection = false
+    panel.allowedFileTypes = ["qfx"]
+    return panel
+  }()
+  
+  //----------------------------------------------------------------------------------------
+  // sourceListViewController()
+  //----------------------------------------------------------------------------------------
+  func sourceListViewController() -> SourceListViewController {
+    let leftSplitViewItem = self.splitViewItems[0]
+    let sourceListVC      = leftSplitViewItem.viewController as! SourceListViewController
+    sourceListVC.delegate = self
+    
+    return sourceListVC
+  }
+  
+  //----------------------------------------------------------------------------------------
+  // detailViewController()
+  //----------------------------------------------------------------------------------------
+  func detailViewController() -> NSViewController {
+    let rightSplitViewItem = self.splitViewItems[1]
+    return rightSplitViewItem.viewController
+  }
+  
+  // MARK: - Lifecycle
+  
+  //----------------------------------------------------------------------------------------
+  // viewDidLoad()
+  //----------------------------------------------------------------------------------------
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Set up source list
+    let numbers = SourceListItem(title: "Numbers", asGroupItem: true)
+    
+    let one     = SourceListItem(title: "One", asGroupItem: false)
+    let two     = SourceListItem(title: "Two", asGroupItem: false)
+    let three   = SourceListItem(title: "Three", asGroupItem: false)
+    
+    one.viewController    = self.storyboard?.instantiateControllerWithIdentifier(self.oneViewControllerID) as? NSViewController
+    two.viewController    = self.storyboard?.instantiateControllerWithIdentifier(self.twoViewControllerID) as? NSViewController
+    three.viewController  = self.storyboard?.instantiateControllerWithIdentifier(self.threeViewControllerID) as? NSViewController
+    
+    numbers.children = [one, two, three]
+    
+    self.sourceListViewController().sourceItems = [numbers]
+    self.sourceListViewController().refresh()
+  }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do view setup here.
+  // MARK: - IBActions
+  
+  //----------------------------------------------------------------------------------------
+  // newBankAccount(sender:)
+  //----------------------------------------------------------------------------------------
+  @IBAction func newBankAccount(sender: AnyObject) {
+    guard let window = NSApplication.sharedApplication().keyWindow else { return }
+    self.openPanel.beginSheetModalForWindow(window, completionHandler: { (response:Int) -> Void in
+      if response == NSModalResponseOK {
+        
+        let url = self.openPanel.URLs[0]
+        print("Add bank account from \(url.path!)")
+//        BankAccountManager.sharedManager.addBankAccountFromStatement(url)
+//        self.refreshSourceList()
+      }
+    })
+  }
+  
+  // MARK: - SourceListViewControllerDelegate
+
+  //----------------------------------------------------------------------------------------
+  // displayViewController(viewController:)
+  //----------------------------------------------------------------------------------------
+  func displayViewController(viewController: NSViewController) {
+    guard self.detailViewController() != viewController else { return }
+    
+    let detailViewController = self.detailViewController()
+    
+    // Remove old view controller
+    if detailViewController.childViewControllers.count > 0 {
+      detailViewController.removeChildViewControllerAtIndex(0)
+      detailViewController.view.subviews[0].removeFromSuperview()
+      detailViewController.view.translatesAutoresizingMaskIntoConstraints = true
     }
     
+    // Add new view controller
+    detailViewController.addChildViewController(viewController)
+    detailViewController.view.addSubview(viewController.view)
+    
+    // Add contraints
+    viewController.view.translatesAutoresizingMaskIntoConstraints = false
+    let views = ["view": viewController.view]
+    
+    let horzContraint = NSLayoutConstraint.constraintsWithVisualFormat("H:|[view]|", options: [], metrics: nil, views: views)
+    let vertContraint = NSLayoutConstraint.constraintsWithVisualFormat("V:|[view]|", options: [], metrics: nil, views: views)
+    
+    NSLayoutConstraint.activateConstraints(horzContraint)
+    NSLayoutConstraint.activateConstraints(vertContraint)
+  }
 }
